976ae7cca99fd151ed4e26a03846d2686f9e146c
Shrine <nischalthapa10@hotmail.com>
2023-06-12 17:23:49 +0545

    TODO list upadted

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 from tqdm import tqdm
 from time import sleep
-#TODO  -- create a Student management system with a simple functionality for assigning grades  asses grades to max min and average and view them
+
+# TODO  -- create a Student management system with a simple functionality for assigning grades  asses grades to max min and average and view them
+# TODO -- Recursively call the student login function to store the student details in the list student list 
+# ! -- student login  recursion required , student logout required a simple break might suffice
 
 #  *model
 
 class Grades:
-    def __init__(self,Grades=""):
+    def __init__(self,subject,Grades=""):
         self.__Grades =Grades
+        self.__subject = subject
     
-    def GiveGrades(self,Grades):
+    def GiveGrades(self):
         self.__Grades = Grades
+        
+    def setGrades(self,grades):
+        self.__Grades = grades
 
             
 class Student(Grades):
@@ -53,8 +60,10 @@ class StudentView:
     
     def Grades(self):
        print("the grades") 
+       
     def Grades_maximum(self):
         print("maximum grades")
+        
     def Grades_average(self):
         print("Average grades")
         
@@ -81,11 +90,14 @@ class StudentLog:
             student_model.setname(self.name)
             self.age  = input("Age :")
             student_model.setage(self.age)
+            return student_model ## student object return 
             
         except ValueError:
             print("name not valid ")
+            
         finally:
             print(f"Name : {student_model.getname}\nAge : {student_model.getage}")
+        
             
 class GradesAsseser:
     def __init__(self):
@@ -99,12 +111,14 @@ def main():
     log = StudentLog()
     checker = input("do you want to login ( Y / n ) : ")
     if tocheck:= checker == "Y" or checker == 'y':
-        log.Login(tocheck)
+        student_list=list(log.Login(tocheck))
     else :
         log.Login(tocheck)
         # GA.Login(False)    
 
-main()
+
+if __name__ == "__main__":
+    main()
         
 
         
